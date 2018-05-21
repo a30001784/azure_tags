5a164d85bc1fe08660e14217b9f5f6e81ae6002a
Lukas Bartsch <lbartsch@agl.com.au>
2018-05-21 14:34:51 +1000

    Renaming script files

---
+++ ansible/roles/common/scripts/Format-Volume_WS2012R2.ps1
@@ -0,0 +1,1 @@
+../../../../scripts/Format-Volume_WS2012R2.ps1
---
+++ scripts/Format-Volume_WS2008R2.ps1
@@ -0,0 +1,11 @@
+$Disks = Get-WmiObject Win32_DiskDrive -Filter "Partitions = 0"
+
+If ( ($Disks | Measure-Object).Count -eq 0) {
+    [Environment]::Exit(0)
+}
+
+Foreach ($disk in $Disks) { 
+   $disk.DeviceID
+   $disk.Index
+   "select disk "+$disk.Index+"`r clean`r create partition primary`r format fs=ntfs unit=65536 quick`r active`r assign letter=F" | diskpart
+}
