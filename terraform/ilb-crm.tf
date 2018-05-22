24d64f5a8fd3f9cc9c7b2067824be81ba2560fed
Lukas Bartsch <lbartsch@agl.com.au>
2018-05-22 11:15:02 +1000

    Updated load balancer config

--- terraform/ilb-crm.tf
+++ terraform/ilb-crm.tf
@@ -25,6 +25,8 @@ resource "azurerm_lb_probe" "ilb-crm-probe" {
     loadbalancer_id                   = "${azurerm_lb.ilb-crm.id}"
     name                              = "${var.load_balancer_probe_prefix_app_crm}80${count.index + 1}0"
     port                              = "80${count.index + 1}0"
+    interval_in_seconds               = 5
+    number_of_probes                  = 2
     depends_on                        = ["azurerm_lb.ilb-crm"]
 }
 
@@ -36,9 +38,10 @@ resource "azurerm_lb_rule" "ilb-crm-rule" {
     protocol                          = "tcp"
     frontend_port                     = "80${count.index + 1}0"
     backend_port                      = "80${count.index + 1}0"
-    idle_timeout_in_minutes           = 3
+    idle_timeout_in_minutes           = 4
+    load_distribution                 = "SourceIPProtocol"
     frontend_ip_configuration_name    = "${var.load_balancer_frontend_config_name_app_crm}"
-    enable_floating_ip                = true
+    enable_floating_ip                = false
     probe_id                          = "${azurerm_lb_probe.ilb-crm-probe.*.id[count.index]}"
     backend_address_pool_id           = "${azurerm_lb_backend_address_pool.ilb-crm-be-pool.id}"
     depends_on                        = ["azurerm_lb_backend_address_pool.ilb-crm-be-pool","azurerm_lb_probe.ilb-crm-probe"]
