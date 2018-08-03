17ebd9829b94108db9718966635f55ae56c6f510
Lukas Bartsch <lbartsch@agl.com.au>
2018-08-03 14:38:47 +1000

    Added extra ASCS and database servers

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,23 +1,65 @@
-output "ip_addresses_app" { 
-    value = "${concat(azurerm_network_interface.app-crm.*.private_ip_address,azurerm_network_interface.app-isu.*.private_ip_address)}"
+output "ip_addresses_app-crm" { 
+    value = "${azurerm_network_interface.app-crm.*.private_ip_address}"
 }
 
-// output "ip_addresses_ascs" { 
-//     value = ["${azurerm_network_interface.ascs.*.private_ip_address}"]
-// }
+output "ip_addresses_app-isu" { 
+    value = "${azurerm_network_interface.app-isu.*.private_ip_address}"
+}
+
+output "ip_addresses_app-nwgw" { 
+    value = "${azurerm_network_interface.app-nwgw.*.private_ip_address}"
+}
+
+output "ip_addresses_app-xi" { 
+    value = "${azurerm_network_interface.app-xi.*.private_ip_address}"
+}
+
+output "ip_addresses_ascs-crm" { 
+    value = "${element(azurerm_network_interface.ascs.*.private_ip_address, 0)}"
+}
+
+output "ip_addresses_ascs-isu" {
+    value = "${element(azurerm_network_interface.ascs.*.private_ip_address, 1)}"
+}
+
+output "ip_addresses_ascs-pi" {
+    value = ["${azurerm_network_interface.ascs-pi.*.private_ip_address}"]
+}
+
+output "ip_addresses_ascs-xi" {
+    value = ["${azurerm_network_interface.ascs-xi.*.private_ip_address}"]
+}
 
 // output "ip_addresses_data" { 
 //     value = ["${azurerm_network_interface.db-isu.private_ip_address}","${azurerm_network_interface.db-crm.private_ip_address}"]
 // }
 
-// output "ip_addresses_db-isu" { 
+output "ip_addresses_data-crm" { 
+    value = ["${azurerm_network_interface.db-crm.private_ip_address}"]
+}
+
+// output "ip_addresses_data-isu" { 
 //     value = ["${azurerm_network_interface.db-isu.private_ip_address}"]
 // }
 
-output "ip_addresses_db-crm" { 
-    value = ["${azurerm_network_interface.db-crm.private_ip_address}"]
+output "ip_addresses_data-nwgw" { 
+    value = ["${azurerm_network_interface.db-nwgw.*.private_ip_address}"]
+} 
+
+output "ip_addresses_data-pi" { 
+    value = ["${azurerm_network_interface.db-pi.*.private_ip_address}"]
+}
+
+output "ip_addresses_data-xi" {
+    value = ["${azurerm_network_interface.db-xi.*.private_ip_address}"]
 }
 
 // output "ip_addresses_pi" {
 //     value = ["${azurerm_network_interface.pi.*.private_ip_address}"]
+// }
+
+// output "ip_addresses_data" { 
+//     value = {
+        
+//     }
 // }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -46,6 +46,20 @@ variable "availability_set_name_app_isu" {}
 variable "hostname_suffix_start_range_app_isu" {}
 variable "node_count_app_isu" {}
 
+## ==== APP NWGW =========================================================== ##
+
+variable "availability_set_name_app_nwgw" {}
+variable "hostname_suffix_start_range_app_nwgw" {}
+variable "node_count_app_nwgw" {}
+variable "vm_size_app_nwgw" {} 
+
+## ==== APP XI ============================================================== ##
+
+variable "availability_set_name_app_xi" {}
+variable "hostname_suffix_start_range_app_xi" {}
+variable "node_count_app_xi" {}
+variable "vm_size_app_xi" {} 
+
 ## ==== DB ================================================================= ##
 
 variable "vm_size_db" {}
@@ -57,22 +71,53 @@ variable "data_disk_size_db" {}
 
 variable "hostname_suffix_start_range_db_isu" {}
 variable "data_disk_count_db_isu" {}
+variable "node_count_db_isu" {}
 
 ## ==== DB CRM ============================================================= ##
 
 variable "hostname_suffix_start_range_db_crm" {}
 variable "data_disk_count_db_crm" {}
+variable "node_count_db_crm" {}
 
-## ==== ASCS =============================================================== ##
+## ==== DB NWGW ============================================================= ##
+
+variable "hostname_suffix_start_range_db_nwgw" {}
+variable "data_disk_count_db_nwgw" {}
+variable "vm_size_db_nwgw" {}
+variable "node_count_db_nwgw" {}
+
+## ==== DB PI =============================================================== ##
+
+variable "hostname_suffix_start_range_db_pi" {}
+variable "data_disk_count_db_pi" {}
+variable "vm_size_db_pi" {}
+variable "node_count_db_pi" {}
+
+## ==== DB XI =============================================================== ##
+
+variable "hostname_suffix_start_range_db_xi" {}
+variable "data_disk_count_db_xi" {}
+variable "vm_size_db_xi" {}
+variable "node_count_db_xi" {}
+
+## ==== ASCS CRM/ISU ======================================================== ##
 
 variable "node_count_ascs" {}
 variable "vm_size_ascs" {} 
 variable "hostname_suffix_start_range_ascs" {}
 variable "data_disk_size_ascs" {}
 
-## ==== PI ================================================================= ##
+## ==== ASCS PI ============================================================= ##
 
-variable "vm_size_pi" {}
-variable "hostname_suffix_start_range_pi" {}
-variable "data_disk_count_pi" {}
-variable "data_disk_size_pi" {}
+variable "availability_set_name_ascs_pi" {}
+variable "data_disk_size_ascs_pi" {}
+variable "hostname_suffix_start_range_ascs_pi" {}
+variable "node_count_ascs_pi" {}
+variable "vm_size_ascs_pi" {} 
+
+## ==== ASCS XI ============================================================= ##
+
+variable "node_count_ascs_xi" {}
+variable "vm_size_ascs_xi" {} 
+variable "hostname_suffix_start_range_ascs_xi" {}
+variable "data_disk_size_ascs_xi" {}
