4ee9f71882552e1df896596a8a89d6eaa4a9c3f8
Lukas Bartsch <lbartsch@agl.com.au>
2018-10-29 15:51:26 +1100

    Testing file share uri

--- scripts/Download-SAPFiles.ps1
+++ scripts/Download-SAPFiles.ps1
@@ -16,6 +16,8 @@ Param
 
 $ErrorActionPreference = "Stop"
 
+$FileShareUri = "\\aaasapautomationsa.file.core.windows.net\sapptia"
+
 $Key = ConvertTo-SecureString -String $StorageAccountKey -AsPlainText -Force
 $Credentials = New-Object System.Management.Automation.PSCredential -ArgumentList "Azure\$($StorageAccountName)", $Key
 New-PSDrive -Name X -PSProvider FileSystem -Root $FileShareUri -Credential $Credentials
