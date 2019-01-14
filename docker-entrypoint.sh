b06932966ac88f3f0c94d7b6cffc07b8cd83e4a1
A144395 <A144395@agl.com.au>
2019-01-14 15:07:34 +1100

    tf appply

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -102,7 +102,7 @@ sed -i "s#RESOURCE_GROUP_NAME#${TF_VARS['TF_VAR_resource_group_name']}#g" "${ter
 cd "${terraform_dir}" && \
     terraform init && \
     terraform plan #&& \
-#     terraform apply --auto-approve
+    terraform apply --auto-approve
 
 if [ $? -ne "0" ]; then
     echo "[ERROR] Fatal error encountered in Terraform run"
