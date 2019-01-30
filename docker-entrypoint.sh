87919acde4e151b500af2296b7f7a78aeb41a504
Lukas Bartsch <lbartsch@agl.com.au>
2019-01-30 16:13:49 +1100

    Fixed typo

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -261,7 +261,8 @@ echo "[INFO] Beginning Ansible playbooks..."
 
 cat "${inventory_file}"
 
-playbooks=( "configure-app" )# "configure-crm-java" )
+playbooks=( "configure-app" ) 
+# "configure-crm-java"
 
 cd "${ansible_dir}"
 for playbook in "${playbooks[@]}"; do
