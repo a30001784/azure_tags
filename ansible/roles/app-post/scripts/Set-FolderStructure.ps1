e73d82fa188b81105b7abe19a4d0c4612b795221
Lukas Bartsch <>
2019-03-27 12:13:00 +1100

    Comments

--- ansible/roles/app-post/scripts/Set-FolderStructure.ps1
+++ ansible/roles/app-post/scripts/Set-FolderStructure.ps1
@@ -12,12 +12,17 @@ $shares = "Archive","Interface"
 
 # Copy folder structure only
 foreach ($share in $shares) {
+    # Traverse network share
     Get-ChildItem -Path ("\\{0}\{1}" -f $Source, $share) -Recurse -Directory | `
+        # Exclude scripts directories
         Where-Object { $_.FullName -inotmatch "scripts" } | `
             ForEach-Object {
+                # Replace UNC path with local path
                 $newPath = ($_.FullName).Replace(("\\{0}" -f $Source),("{0}:" -f $DriveLetter))
+                # Create directory
                 New-Item -Path $newPath -ItemType Directory
             }
+    # Create SMB share
     New-SmbShare -Name $share -Path ("{0}:\{1}" -f $DriveLetter,$share)
 }
 
