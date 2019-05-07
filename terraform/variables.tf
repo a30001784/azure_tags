426d4dba2d11172066a2d1637424209480eec63e
Lukas Bartsch <>
2019-05-07 16:59:48 +1000

    Added outputs and variables. Updated gitignore

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,120 +1,50 @@
-## ==== Provider =========================================================== ##
-
 variable "tenant_id" {}
 variable "subscription_id" {}
 variable "client_id" {}
 variable "client_secret" {}
 
-## ==== Tags =============================================================== ## 
-
-variable "tag_business_owner" {}
-variable "tag_cost_code" {}
-variable "tag_technical_owner" {}
-
-## ==== Shared ============================================================= ##
-
 variable "resource_group_name" {}
 variable "location" {}
-variable "host_username" {}
+
 variable "host_password" {}
+
 variable "hostname_prefix" {}
+variable "hostname_suffix_start_range" {}
 
-## ==== App ================================================================ ##
+variable "subnet_id" {}
 
-variable "vm_size_app" {}
-variable "network_security_group_app" {}
-variable "subnet_id_app" {}
+variable "node_count_crm_app" {
+    default = 0
+}
+variable "node_count_crm_ascs" { 
+    default = 0
+}
+variable "node_count_crm_db" { 
+    default = 0
+}
 
-## ==== App CRM ============================================================ ##
+variable "data_disk_size_secondary_crm_app" {
+    default = 512
+}
+variable "data_disk_count_secondary_crm_app" {
+    default = 0
+}
+variable "data_disk_size_secondary_crm_ascs" {
+    default = 512
+}
+variable "data_disk_count_secondary_crm_ascs" {
+    default = 0
+}
+variable "data_disk_size_secondary_crm_db" {
+    default = 512
+}
+variable "data_disk_count_secondary_crm_db" {
+    default = 0
+}
 
-variable "availability_set_name_app_crm" {}
-variable "hostname_suffix_start_range_app_crm" {}
-variable "node_count_app_crm" {}
+variable "vm_size_crm_app" {}
+variable "vm_size_crm_ascs" {}
+variable "vm_size_crm_db" {}
 
-## ==== App ISU ============================================================ ##
-
-variable "availability_set_name_app_isu" {}
-variable "hostname_suffix_start_range_app_isu" {}
-variable "node_count_app_isu" {}
-
-## ==== APP NWGW =========================================================== ##
-
-variable "availability_set_name_app_nwgw" {}
-variable "hostname_suffix_start_range_app_nwgw" {}
-variable "node_count_app_nwgw" {}
-variable "vm_size_app_nwgw" {} 
-
-## ==== APP SWD ============================================================= ##
-
-variable "hostname_suffix_start_range_app_swd" {}
-variable "node_count_app_swd" {}
-variable "vm_size_app_swd" {}
-
-## ==== APP XI ============================================================== ##
-
-variable "availability_set_name_app_xi" {}
-variable "hostname_suffix_start_range_app_xi" {}
-variable "node_count_app_xi" {}
-variable "vm_size_app_xi" {} 
-
-## ==== DB ================================================================= ##
-
-variable "vm_size_db" {}
-variable "network_security_group_data" {}
-variable "subnet_id_data" {}
-variable "data_disk_size_db" {}
-
-## ==== DB ISU ============================================================= ##
-
-variable "hostname_suffix_start_range_db_isu" {}
-variable "data_disk_count_db_isu" {}
-variable "node_count_db_isu" {}
-
-## ==== DB CRM ============================================================= ##
-
-variable "hostname_suffix_start_range_db_crm" {}
-variable "data_disk_count_db_crm" {}
-variable "node_count_db_crm" {}
-
-## ==== DB NWGW ============================================================= ##
-
-variable "hostname_suffix_start_range_db_nwgw" {}
-variable "data_disk_count_db_nwgw" {}
-variable "vm_size_db_nwgw" {}
-variable "node_count_db_nwgw" {}
-
-## ==== DB PI =============================================================== ##
-
-variable "hostname_suffix_start_range_db_pi" {}
-variable "data_disk_count_db_pi" {}
-variable "vm_size_db_pi" {}
-variable "node_count_db_pi" {}
-
-## ==== DB XI =============================================================== ##
-
-variable "hostname_suffix_start_range_db_xi" {}
-variable "data_disk_count_db_xi" {}
-variable "vm_size_db_xi" {}
-variable "node_count_db_xi" {}
-
-## ==== ASCS CRM/ISU ======================================================== ##
-
-variable "node_count_ascs" {}
-variable "vm_size_ascs" {} 
-variable "hostname_suffix_start_range_ascs" {}
-variable "data_disk_size_ascs" {}
-
-## ==== ASCS PI ============================================================= ##
-
-variable "availability_set_name_ascs_pi" {}
-variable "data_disk_size_ascs_pi" {}
-variable "hostname_suffix_start_range_ascs_pi" {}
-variable "node_count_ascs_pi" {}
-variable "vm_size_ascs_pi" {} 
-
-## ==== ASCS XI ============================================================= ##
-
-variable "node_count_ascs_xi" {}
-variable "vm_size_ascs_xi" {} 
-variable "hostname_suffix_start_range_ascs_xi" {}
-variable "data_disk_size_ascs_xi" {}
+variable "nsg_name_app" {}
+variable "nsg_name_data" {}
