474d63070b0ba4ad344dad20db5c8f33315f8df4
Lukas Bartsch <>
2019-05-08 15:05:41 +1000

    Added TF deploy script

---
+++ terraform/tf_deploy.sh
@@ -0,0 +1,14 @@
+echo "***Initialising Terraform***"
+terraform init \
+    -backend-config="access_key=${ARM_ACCESS_KEY}" \
+    -backend-config="storage_account_name=${ARM_ACCESS_SA}" \
+    -backend-config="resource_group_name=${RESOURCE_GROUP}"
+
+echo "***Running terraform command***"
+if [ "$1" == "plan" ]; then
+    terraform plan
+fi
+
+if [ "$1" == "apply" ]; then
+    terraform apply -auto-approve
+fi
