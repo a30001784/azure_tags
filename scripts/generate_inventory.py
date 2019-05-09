6d449630fad2f0cbfa5c68699a69f3cc3e9c0f18
Lukas Bartsch <>
2019-05-09 19:56:13 +1000

    More error checks. Cleaner handling of file and folder paths

--- scripts/generate_inventory.py
+++ scripts/generate_inventory.py
@@ -7,21 +7,30 @@ def main():
     parser = argparse.ArgumentParser(description="Generate Ansible inventory file")
     parser.add_argument("-r", "--roles", nargs="+", type=str, required=True, help="List of roles", default=[])
     parser.add_argument("-s", "--sub-roles", nargs="+", type=str, required=True, help="List of sub roles", default=[])
-    parser.add_argument("-o", "--out-file", type=str, required=True, help="File path to output inventory file")
+    parser.add_argument("-i", "--inv-dir", type=str, required=True, help="Relative file path to location to store inventory file. Must be a directory.")
     parser.add_argument("-t", "--tf-path", type=str, required=True, help="Relative path from working directory to Terraform directory")
     args = parser.parse_args()
 
-    # Change to Terraform working directory
-    if os.path.exists(args.tf_path):
-        os.chdir(args.tf_path)
-    else:
-        print("Failed to enter Terraform directory: {}".format(args.tf_path))
+    # Check for valid TF directory
+    if not os.path.exists(args.tf_path):
+        print("Specified path not valid Terraform directory: {}".format(args.tf_path))
         exit(1)
 
-    generate_file(args.roles, args.sub_roles, args.out_file)
+    # Check for valid directory for inventory file
+    if not os.path.isdir(args.inv_dir):
+        print("Invalid directory for Ansible inventory file: {}".format(args.inv_dir))
+        exit(2)
 
-def generate_file(roles, sub_roles, out_file):
-    f = open(out_file, "w+")
+    # Get absolute file path for inventory file
+    inv_dir = os.path.abspath(args.inv_dir)
+
+    # Change to Terraform working directory
+    os.chdir(args.tf_path)
+
+    generate_file(args.roles, args.sub_roles, inv_dir)
+
+def generate_file(roles, sub_roles, inv_dir):
+    f = open("{}/inventory.txt".format(inv_dir), "w+")
 
     for role in roles:
         children = []
