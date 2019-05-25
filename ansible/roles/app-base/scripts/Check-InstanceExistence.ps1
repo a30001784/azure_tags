b372b32ca3cc471e70211368a64379bce05a1478
Lukas Bartsch <>
2019-05-25 14:43:48 +1000

    Refining the check for existing installation

--- ansible/roles/app-base/scripts/Check-InstanceExistence.ps1
+++ ansible/roles/app-base/scripts/Check-InstanceExistence.ps1
@@ -11,7 +11,18 @@ Param
     [int]$TargetInstanceNumber
 )
 
-$InstanceList = & "C:\\Program Files\\SAP\\hostctrl\\exe\\sapcontrol.exe" `
+$ErrorActionPreference = "Stop"
+
+$SapControlExe = "C:\Program Files\SAP\hostctrl\exe\sapcontrol.exe"
+
+if (-Not (Test-Path $SapControlExe)) {
+    # No instances have been installed if `sapcontrol.exe` doesn't exist
+    Write-Host "false"
+    Write-Host "sapcontrol.exe does not exist on the system"
+    [Environment]::Exit(0)
+}
+
+$InstanceList = & $SapControlExe `
     -nr 00 `
     -host "$($ASCSHost)" `
     -function GetSystemInstanceList
@@ -24,7 +35,7 @@ if ($InstanceList | Select-String -CaseSensitive "FAIL") {
 
 # Successfully retrieved system instance list
 if ($InstanceList | Select-String -CaseSensitive "OK") {
-    
+        
     # Instance exists
     if ($InstanceList | Select-String "$($TargetHost), $($TargetInstanceNumber)" | Select-String "GREEN") {
         Write-Host "true"
@@ -35,6 +46,7 @@ if ($InstanceList | Select-String -CaseSensitive "OK") {
         Write-Host "false"
     }
 }
+
 
 Write-Host "Printing result for debugging information"
 Write-Host $InstanceList
