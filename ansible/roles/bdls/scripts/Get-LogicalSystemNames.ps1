904c3813ee3fc5a8857529e08bdd0fd698f54923
Lukas Bartsch <>
2019-05-27 22:08:39 +1000

    Fix a bunch of crap that wasn't working

--- ansible/roles/bdls/scripts/Get-LogicalSystemNames.ps1
+++ ansible/roles/bdls/scripts/Get-LogicalSystemNames.ps1
@@ -7,34 +7,37 @@ Param
     
     [Parameter(Mandatory=$true)]
     [string]$Schema,
-    
-    [Parameter(Mandatory=$false)]
-    [string]$OutFile = "$($Database).txt"
+
+    [Parameter(Mandatory=$true)]
+    [string]$OutFile = "C:\Windows\Temp\$($Database).txt"
 )
 
 try {
+    $LogicalSystemNames = ""
     $Patterns = @("B%", "M%", "X%", "AGLMDH%")
 
     if (Test-Path $OutFile) {
         Remove-Item $OutFile -Force
     }
 
-    Add-Content $OutFile $(Invoke-Sqlcmd `
+    $LogicalSystemNames+=$(Invoke-Sqlcmd `
         -Database $Database `
         -Query "SELECT LOGSYS FROM $($schema).T000 WHERE MANDT = 100" `
         -IncludeSqlUserErrors `
         -AbortOnError).LOGSYS
 
     foreach ($pattern in $Patterns) {
-        Add-Content $OutFile $(Invoke-Sqlcmd `
+        $LogicalSystemNames+=$(Invoke-Sqlcmd `
             -Database $Database `
-            -Query "SELECT PARNUM from ia5.EDPP1 where PARNUM LIKE '$($pattern)%'" `
+            -Query "SELECT PARNUM from $($schema).EDPP1 where PARNUM LIKE '$($pattern)%'" `
             -IncludeSqlUserErrors `
             -AbortOnError).PARNUM            
     }
+
+    Add-Content $OutFile $LogicalSystemNames
 }
 catch {
-    Write-Error "Error occured:"
-    Write-Error $_
+    Add-Content "C:\Windows\Temp\error.log" "Error occured:"
+    Add-Content "C:\Windows\Temp\error.log" $_
     [Environment]::Exit(1)
 }
