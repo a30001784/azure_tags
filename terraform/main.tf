cbc9bbccee4fc5bb35bd8f0ac84b6d80c5c10dff
Simon Kirby <a30001734@agl.com.au>
2019-05-31 11:10:11 +1000

    Updating BDLS - fixing terraform breaking changes

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,134 +1,126 @@
 terraform {
   backend "azurerm" {
-    container_name                       = "terraform-state"
-    key                                  = "terraform.tfstate"
+    container_name = "terraform-state"
+    key            = "terraform.tfstate"
   }
 }
-    var_what_ever_i_called_it = "string like this"
-    someother_var_name = ${var.var_what_ever_i_called_it}
+
 provider "azurerm" {
-    subscription_id                      = "${var.subscription_id}"
-    client_id                            = "${var.client_id}"
-    client_secret                        = "${var.client_secret}"
-    tenant_id                            = "${var.tenant_id}"
+  subscription_id = "${var.subscription_id}"
+  client_id       = "${var.client_id}"
+  client_secret   = "${var.client_secret}"
+  tenant_id       = "${var.tenant_id}"
 }
 
 locals {
-    hostname_suffix_start_range_crm_app  = "${var.hostname_suffix_start_range}"
-    hostname_suffix_start_range_crm_ascs = "${local.hostname_suffix_start_range_crm_app + var.node_count_crm_app}"
-    hostname_suffix_start_range_crm_db   = "${local.hostname_suffix_start_range_crm_ascs + var.node_count_crm_ascs}"
-    hostname_suffix_start_range_isu_app  = "${local.hostname_suffix_start_range_crm_db + var.node_count_crm_db}"
-    hostname_suffix_start_range_isu_ascs = "${local.hostname_suffix_start_range_isu_app + var.node_count_isu_app}"
-    hostname_suffix_start_range_isu_db   = "${local.hostname_suffix_start_range_isu_ascs + var.node_count_isu_ascs}"
-    asdasdsadasdasdasdasd = ${var.var_what_ever_i_called_it}
-    aasdasdasdsad = ${local.response.name}
+  hostname_suffix_start_range_crm_app  = "${var.hostname_suffix_start_range}"
+  hostname_suffix_start_range_crm_ascs = "${local.hostname_suffix_start_range_crm_app + var.node_count_crm_app}"
+  hostname_suffix_start_range_crm_db   = "${local.hostname_suffix_start_range_crm_ascs + var.node_count_crm_ascs}"
+  hostname_suffix_start_range_isu_app  = "${local.hostname_suffix_start_range_crm_db + var.node_count_crm_db}"
+  hostname_suffix_start_range_isu_ascs = "${local.hostname_suffix_start_range_isu_app + var.node_count_isu_app}"
+  hostname_suffix_start_range_isu_db   = "${local.hostname_suffix_start_range_isu_ascs + var.node_count_isu_ascs}"
 }
 
-response : {
-    { name= thing },
-    { test = secondthing}
+module "nsg-app" {
+  source         = "./modules/network-security-group"
+  name           = "${var.network_security_group_name_app}"
+  resource_group = "${var.resource_group_name}"
+  location       = "${var.location}"
 }
 
-module "nsg-app" { 
-    source                               = "./modules/network-security-group"
-    name                                 = "${var.network_security_group_name_app}"
-    resource_group                       = "${var.resource_group_name}"
-    location                             = "${var.location}"
+module "nsg-data" {
+  source         = "./modules/network-security-group"
+  name           = "${var.network_security_group_name_data}"
+  resource_group = "${var.resource_group_name}"
+  location       = "${var.location}"
 }
 
-module "nsg-data" { 
-    source                               = "./modules/network-security-group"
-    name                                 = "${var.network_security_group_name_data}"
-    resource_group                       = "${var.resource_group_name}"
-    location                             = "${var.location}"
-}
-
-module "crm-app" { 
-    source                               = "./modules/windows-vm"
-    resource_group                       = "${var.resource_group_name}"
-    location                             = "${var.location}"
-    count                                = "${var.node_count_crm_app}"
-    hostname_prefix                      = "${var.hostname_prefix}"
-    hostname_suffix_start_range          = "${local.hostname_suffix_start_range_crm_app}"
-    subnet_id                            = "${var.subnet_id_app}"
-    host_password                        = "${var.host_password}"
-    vm_size                              = "${var.vm_size_crm_app}"
-    network_security_group_id            = "${module.nsg-app.network_security_group_id}"
-    data_disk_size                       = "${var.data_disk_size_crm_app}"
-    data_disk_count                      = "${var.data_disk_count_crm_app}"
+module "crm-app" {
+  source                      = "./modules/windows-vm"
+  resource_group              = "${var.resource_group_name}"
+  location                    = "${var.location}"
+  count                       = "${var.node_count_crm_app}"
+  hostname_prefix             = "${var.hostname_prefix}"
+  hostname_suffix_start_range = "${local.hostname_suffix_start_range_crm_app}"
+  subnet_id                   = "${var.subnet_id_app}"
+  host_password               = "${var.host_password}"
+  vm_size                     = "${var.vm_size_crm_app}"
+  network_security_group_id   = "${module.nsg-app.network_security_group_id}"
+  data_disk_size              = "${var.data_disk_size_crm_app}"
+  data_disk_count             = "${var.data_disk_count_crm_app}"
 }
 
 module "crm-ascs" {
-    source                               = "./modules/windows-vm"
-    resource_group                       = "${var.resource_group_name}"
-    location                             = "${var.location}"
-    count                                = "${var.node_count_crm_ascs}"
-    hostname_prefix                      = "${var.hostname_prefix}"
-    hostname_suffix_start_range          = "${local.hostname_suffix_start_range_crm_ascs}"
-    subnet_id                            = "${var.subnet_id_app}"
-    host_password                        = "${var.host_password}"
-    vm_size                              = "${var.vm_size_crm_ascs}"
-    network_security_group_id            = "${module.nsg-app.network_security_group_id}"
-    data_disk_size                       = "${var.data_disk_size_crm_ascs}"
-    data_disk_count                      = "${var.data_disk_count_crm_ascs}"
+  source                      = "./modules/windows-vm"
+  resource_group              = "${var.resource_group_name}"
+  location                    = "${var.location}"
+  count                       = "${var.node_count_crm_ascs}"
+  hostname_prefix             = "${var.hostname_prefix}"
+  hostname_suffix_start_range = "${local.hostname_suffix_start_range_crm_ascs}"
+  subnet_id                   = "${var.subnet_id_app}"
+  host_password               = "${var.host_password}"
+  vm_size                     = "${var.vm_size_crm_ascs}"
+  network_security_group_id   = "${module.nsg-app.network_security_group_id}"
+  data_disk_size              = "${var.data_disk_size_crm_ascs}"
+  data_disk_count             = "${var.data_disk_count_crm_ascs}"
 }
 
 module "crm-db" {
-    source                               = "./modules/windows-vm"
-    resource_group                       = "${var.resource_group_name}"
-    location                             = "${var.location}"
-    count                                = "${var.node_count_crm_db}"
-    hostname_prefix                      = "${var.hostname_prefix}"
-    hostname_suffix_start_range          = "${local.hostname_suffix_start_range_crm_db}"
-    subnet_id                            = "${var.subnet_id_data}"
-    host_password                        = "${var.host_password}"
-    vm_size                              = "${var.vm_size_crm_db}"
-    network_security_group_id            = "${module.nsg-data.network_security_group_id}"
-    data_disk_size                       = "${var.data_disk_size_crm_db}"
-    data_disk_count                      = "${var.data_disk_count_crm_db}"
+  source                      = "./modules/windows-vm"
+  resource_group              = "${var.resource_group_name}"
+  location                    = "${var.location}"
+  count                       = "${var.node_count_crm_db}"
+  hostname_prefix             = "${var.hostname_prefix}"
+  hostname_suffix_start_range = "${local.hostname_suffix_start_range_crm_db}"
+  subnet_id                   = "${var.subnet_id_data}"
+  host_password               = "${var.host_password}"
+  vm_size                     = "${var.vm_size_crm_db}"
+  network_security_group_id   = "${module.nsg-data.network_security_group_id}"
+  data_disk_size              = "${var.data_disk_size_crm_db}"
+  data_disk_count             = "${var.data_disk_count_crm_db}"
 }
 
-module "isu-app" { 
-    source                               = "./modules/windows-vm"
-    resource_group                       = "${var.resource_group_name}"
-    location                             = "${var.location}"
-    count                                = "${var.node_count_isu_app}"
-    hostname_prefix                      = "${var.hostname_prefix}"
-    hostname_suffix_start_range          = "${local.hostname_suffix_start_range_isu_app}"
-    subnet_id                            = "${var.subnet_id_app}"
-    host_password                        = "${var.host_password}"
-    vm_size                              = "${var.vm_size_isu_app}"
-    network_security_group_id            = "${module.nsg-app.network_security_group_id}"
-    data_disk_size                       = "${var.data_disk_size_isu_app}"
-    data_disk_count                      = "${var.data_disk_count_isu_app}"
+module "isu-app" {
+  source                      = "./modules/windows-vm"
+  resource_group              = "${var.resource_group_name}"
+  location                    = "${var.location}"
+  count                       = "${var.node_count_isu_app}"
+  hostname_prefix             = "${var.hostname_prefix}"
+  hostname_suffix_start_range = "${local.hostname_suffix_start_range_isu_app}"
+  subnet_id                   = "${var.subnet_id_app}"
+  host_password               = "${var.host_password}"
+  vm_size                     = "${var.vm_size_isu_app}"
+  network_security_group_id   = "${module.nsg-app.network_security_group_id}"
+  data_disk_size              = "${var.data_disk_size_isu_app}"
+  data_disk_count             = "${var.data_disk_count_isu_app}"
 }
 
 module "isu-ascs" {
-    source                               = "./modules/windows-vm"
-    resource_group                       = "${var.resource_group_name}"
-    location                             = "${var.location}"
-    count                                = "${var.node_count_isu_ascs}"
-    hostname_prefix                      = "${var.hostname_prefix}"
-    hostname_suffix_start_range          = "${local.hostname_suffix_start_range_isu_ascs}"
-    subnet_id                            = "${var.subnet_id_app}"
-    host_password                        = "${var.host_password}"
-    vm_size                              = "${var.vm_size_isu_ascs}"
-    network_security_group_id            = "${module.nsg-app.network_security_group_id}"
-    data_disk_size                       = "${var.data_disk_size_isu_ascs}"
-    data_disk_count                      = "${var.data_disk_count_isu_ascs}"
+  source                      = "./modules/windows-vm"
+  resource_group              = "${var.resource_group_name}"
+  location                    = "${var.location}"
+  count                       = "${var.node_count_isu_ascs}"
+  hostname_prefix             = "${var.hostname_prefix}"
+  hostname_suffix_start_range = "${local.hostname_suffix_start_range_isu_ascs}"
+  subnet_id                   = "${var.subnet_id_app}"
+  host_password               = "${var.host_password}"
+  vm_size                     = "${var.vm_size_isu_ascs}"
+  network_security_group_id   = "${module.nsg-app.network_security_group_id}"
+  data_disk_size              = "${var.data_disk_size_isu_ascs}"
+  data_disk_count             = "${var.data_disk_count_isu_ascs}"
 }
 
 module "isu-db" {
-    source                               = "./modules/windows-vm"
-    resource_group                       = "${var.resource_group_name}"
-    location                             = "${var.location}"
-    count                                = "${var.node_count_isu_db}"
-    hostname_prefix                      = "${var.hostname_prefix}"
-    hostname_suffix_start_range          = "${local.hostname_suffix_start_range_isu_db}"
-    subnet_id                            = "${var.subnet_id_data}"
-    host_password                        = "${var.host_password}"
-    vm_size                              = "${var.vm_size_isu_db}"
-    network_security_group_id            = "${module.nsg-data.network_security_group_id}"
-    data_disk_size                       = "${var.data_disk_size_isu_db}"
-    data_disk_count                      = "${var.data_disk_count_isu_db}"
+  source                      = "./modules/windows-vm"
+  resource_group              = "${var.resource_group_name}"
+  location                    = "${var.location}"
+  count                       = "${var.node_count_isu_db}"
+  hostname_prefix             = "${var.hostname_prefix}"
+  hostname_suffix_start_range = "${local.hostname_suffix_start_range_isu_db}"
+  subnet_id                   = "${var.subnet_id_data}"
+  host_password               = "${var.host_password}"
+  vm_size                     = "${var.vm_size_isu_db}"
+  network_security_group_id   = "${module.nsg-data.network_security_group_id}"
+  data_disk_size              = "${var.data_disk_size_isu_db}"
+  data_disk_count             = "${var.data_disk_count_isu_db}"
 }
--- terraform/modules/windows-vm/main.tf
+++ terraform/modules/windows-vm/main.tf
@@ -1,94 +1,95 @@
 locals {
-  total_data_disks                 = "${var.data_disk_count * var.count}"
+  total_data_disks = "${var.data_disk_count * var.count}"
 }
 
 resource "azurerm_network_interface" "main" {
-  count                            = "${var.count}"
-  name                             = "${format("${var.hostname_prefix}%04d", var.hostname_suffix_start_range + count.index)}-nic"
-  location                         = "${var.location}"
-  resource_group_name              = "${var.resource_group}"
-  network_security_group_id        = "${var.network_security_group_id}"
-  enable_accelerated_networking    = true
+  count                         = "${var.count}"
+  name                          = "${format("${var.hostname_prefix}%04d", var.hostname_suffix_start_range + count.index)}-nic"
+  location                      = "${var.location}"
+  resource_group_name           = "${var.resource_group}"
+  network_security_group_id     = "${var.network_security_group_id}"
+  enable_accelerated_networking = true
 
   ip_configuration {
-    name                           = "${format("${var.hostname_prefix}%04d", var.hostname_suffix_start_range + count.index)}-ipconfig"
-    subnet_id                      = "${var.subnet_id}"
-    private_ip_address_allocation  = "Dynamic"
+    name                          = "${format("${var.hostname_prefix}%04d", var.hostname_suffix_start_range + count.index)}-ipconfig"
+    subnet_id                     = "${var.subnet_id}"
+    private_ip_address_allocation = "Dynamic"
   }
 }
 
 resource "azurerm_managed_disk" "main" {
-  count                            = "${local.total_data_disks}"
-  name                             = "${format("${var.hostname_prefix}%04d", var.hostname_suffix_start_range + (count.index / var.data_disk_count))}-datadisk-${format("%02d", (count.index % var.data_disk_count + 1))}"
-  location                         = "${var.location}"
-  resource_group_name              = "${var.resource_group}"
-  storage_account_type             = "Premium_LRS"
-  create_option                    = "Empty"
-  disk_size_gb                     = "${var.data_disk_size}"
-//   tags                          = "${var.tags}"
+  count                = "${local.total_data_disks}"
+  name                 = "${format("${var.hostname_prefix}%04d", var.hostname_suffix_start_range + (count.index / var.data_disk_count))}-datadisk-${format("%02d", (count.index % var.data_disk_count + 1))}"
+  location             = "${var.location}"
+  resource_group_name  = "${var.resource_group}"
+  storage_account_type = "Premium_LRS"
+  create_option        = "Empty"
+  disk_size_gb         = "${var.data_disk_size}"
+
+  //   tags                          = "${var.tags}"
 }
 
 resource "azurerm_virtual_machine" "main" {
-  count                            = "${var.count}"
-  name                             = "${format("${var.hostname_prefix}%04d", var.hostname_suffix_start_range + count.index)}"
-  location                         = "${var.location}"
-  resource_group_name              = "${var.resource_group}"
-  network_interface_ids            = ["${azurerm_network_interface.main.*.id[count.index]}"]
-  vm_size                          = "${var.vm_size}"
-  depends_on                       = ["azurerm_network_interface.main"]
+  count                 = "${var.count}"
+  name                  = "${format("${var.hostname_prefix}%04d", var.hostname_suffix_start_range + count.index)}"
+  location              = "${var.location}"
+  resource_group_name   = "${var.resource_group}"
+  network_interface_ids = ["${azurerm_network_interface.main.*.id[count.index]}"]
+  vm_size               = "${var.vm_size}"
+  depends_on            = ["azurerm_network_interface.main"]
 
   delete_os_disk_on_termination    = true
   delete_data_disks_on_termination = true
 
   storage_image_reference {
-    publisher                      = "MicrosoftWindowsServer"
-    offer                          = "WindowsServer"
-    sku                            = "2016-Datacenter"
-    version                        = "latest"
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2016-Datacenter"
+    version   = "latest"
   }
 
   storage_os_disk {
-    name                           = "${format("${var.hostname_prefix}%04d", var.hostname_suffix_start_range + count.index)}-osdisk"
-    caching                        = "ReadWrite"
-    create_option                  = "FromImage"
-    managed_disk_type              = "Premium_LRS"
-    disk_size_gb                   = "${var.os_disk_size}"
+    name              = "${format("${var.hostname_prefix}%04d", var.hostname_suffix_start_range + count.index)}-osdisk"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Premium_LRS"
+    disk_size_gb      = "${var.os_disk_size}"
   }
 
   os_profile {
-    computer_name                  = "${format("${var.hostname_prefix}%04d", var.hostname_suffix_start_range + count.index)}"
-    admin_username                 = "${var.host_username}"
-    admin_password                 = "${var.host_password}"
+    computer_name  = "${format("${var.hostname_prefix}%04d", var.hostname_suffix_start_range + count.index)}"
+    admin_username = "${var.host_username}"
+    admin_password = "${var.host_password}"
   }
 
   os_profile_windows_config {
-    provision_vm_agent             = true
+    provision_vm_agent = true
   }
 
-//   tags                          = "${var.tags}"
+  //   tags                          = "${var.tags}"
 }
 
 resource "azurerm_virtual_machine_data_disk_attachment" "main" {
-  count                            = "${local.total_data_disks}"
-  virtual_machine_id               = "${azurerm_virtual_machine.main.*.id[count.index / var.data_disk_count]}"
-  managed_disk_id                  = "${azurerm_managed_disk.main.*.id[count.index]}"
-  lun                              = "${count.index % var.data_disk_count}"
-  caching                          = "ReadOnly"
-  depends_on                       = ["azurerm_network_interface.main", "azurerm_managed_disk.main"]
+  count              = "${local.total_data_disks}"
+  virtual_machine_id = "${azurerm_virtual_machine.main.*.id[count.index / var.data_disk_count]}"
+  managed_disk_id    = "${azurerm_managed_disk.main.*.id[count.index]}"
+  lun                = "${count.index % var.data_disk_count}"
+  caching            = "ReadOnly"
+  depends_on         = ["azurerm_network_interface.main", "azurerm_managed_disk.main"]
 }
 
 resource "azurerm_virtual_machine_extension" "main" {
-  count                            = "${var.count}"
-  name                             = "${azurerm_virtual_machine.main.*.name[count.index]}_configure_remoting_for_ansible"
-  location                         = "${var.location}"
-  resource_group_name              = "${var.resource_group}"
-  virtual_machine_name             = "${azurerm_virtual_machine.main.*.name[count.index]}"
-  publisher                        = "Microsoft.Compute"
-  type                             = "CustomScriptExtension"
-  type_handler_version             = "1.8"
-  depends_on                       = ["azurerm_virtual_machine.main","azurerm_virtual_machine_data_disk_attachment.main" ]
+  count                = "${var.count}"
+  name                 = "${azurerm_virtual_machine.main.*.name[count.index]}_configure_remoting_for_ansible"
+  location             = "${var.location}"
+  resource_group_name  = "${var.resource_group}"
+  virtual_machine_name = "${azurerm_virtual_machine.main.*.name[count.index]}"
+  publisher            = "Microsoft.Compute"
+  type                 = "CustomScriptExtension"
+  type_handler_version = "1.8"
+  depends_on           = ["azurerm_virtual_machine.main", "azurerm_virtual_machine_data_disk_attachment.main"]
 
-  settings                         = <<SETTINGS
+  settings = <<SETTINGS
     {
         "commandToExecute":          "C:\\Windows\\system32\\WindowsPowerShell\\v1.0\\powershell.exe -ExecutionPolicy Bypass -File ConfigureRemotingForAnsible.ps1",
         "fileUris":                  ["https://raw.githubusercontent.com/ansible/ansible/devel/examples/scripts/ConfigureRemotingForAnsible.ps1"]
